b6b8a34bb57270e442b36df072a6993a44ae1ccc
JDjedi <JDjedi1201@gmail.com>
2016-08-29 19:26:01 -0500

    Ameded contorller file and linked weather index to welcome page

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -7,9 +7,10 @@ class WeatherController < ApplicationController
 		open('http://api.wunderground.com/api/bb60b5fbf63bb327/geolookup/conditions/q/TX/Austin.json') do |f|
 			json_string = f.read
 			parsed_json = JSON.parse(json_string)
-			location = parsed_json['location']['city']
-			temp_f = parsed_json['current_observation']['temp_f']
-			puts "Current temperature in #{location} is: #{temp_f}\n"
+			@location = parsed_json['location']['city']
+			@temp_f = parsed_json['current_observation']['temp_f']
+			@wind_dir = parsed_json['current_observation']['wind_dir']
+			@weather = parsed_json['current_observation']['weather']
 		end
 	end
 end
